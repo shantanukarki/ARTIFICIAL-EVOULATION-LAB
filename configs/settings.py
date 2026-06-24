d13abe4416ec8e155638c69e25408278e7970d07
Ishan4569 <ishan4569@gmail.com>
2026-06-24 18:48:16 +0530

    Added Next Part of the Project the TB Files

--- configs/settings.py
+++ configs/settings.py
@@ -1,38 +1,33 @@
-# ── World ──────────────────────────────────────────────────────────────────────
-<<<<<<< HEAD
-WORLD_WIDTH          = 1200
-WORLD_HEIGHT         = 900
-=======
-WORLD_WIDTH          = 1100
-WORLD_HEIGHT         = 800
->>>>>>> 8841aff5839f1ffc4f9ea198d317dcb11acd11a1
-FPS                  = 30
-BG_COLOR             = (12, 12, 22)
+#--World ──────────────────────────────────────────────────────────────────────
+WORLD_WIDTH = 1100
+WORLD_HEIGHT = 800
+FPS = 30
+BG_COLOR = (12, 12, 22)
 
 # ── Population ─────────────────────────────────────────────────────────────────
-INITIAL_ORGANISMS    = 30
-MAX_ORGANISMS        = 80
-INITIAL_FOOD         = 40
-MAX_AGE              = 3000
+INITIAL_ORGANISMS = 30
+MAX_ORGANISMS = 80
+INITIAL_FOOD = 40
+MAX_AGE = 3000
 
 # ── Food ───────────────────────────────────────────────────────────────────────
-FOOD_SPAWN_RATE      = 0.7
-FOOD_ENERGY          = 45
-FOOD_RADIUS          = 4
-FOOD_COLOR           = (60, 200, 90)
+FOOD_SPAWN_RATE = 0.7
+FOOD_ENERGY = 45
+FOOD_RADIUS = 4
+FOOD_COLOR = (60, 200, 90)
 
 # ── Energy ─────────────────────────────────────────────────────────────────────
-STARTING_ENERGY      = 100.0
-MOVE_ENERGY_BASE     = 0.015
-IDLE_ENERGY_BASE     = 0.01
-REPRODUCTION_COST    = 25.0
-REPRODUCTION_THRESH  = 70.0
+STARTING_ENERGY = 100.0
+MOVE_ENERGY_BASE = 0.015
+IDLE_ENERGY_BASE = 0.01
+REPRODUCTION_COST = 25.0
+REPRODUCTION_THRESH = 70.0
 
 # ── Evolution ──────────────────────────────────────────────────────────────────
-MUTATION_RATE        = 0.7
-MUTATION_STRENGTH    = 0.36
-CROSSOVER_RATE       = 0.5
-ELITISM_COUNT        = 5
+MUTATION_RATE = 0.7
+MUTATION_STRENGTH = 0.36
+CROSSOVER_RATE = 0.5
+ELITISM_COUNT = 5
 
 #── Quantum Mutation ──────────────────────────────────────────────────────────────────
 QUANTUM_ENTROPY = 0.4
